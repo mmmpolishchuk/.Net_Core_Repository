444b5d9ae58cc7de658f2121a808b6544e57eaaa
Михайло Поліщук <55458556+mmmpolishchuk@users.noreply.github.com>
2020-05-27 23:05:51 +0300

    Uploaded homework #1

--- 1.Introduction_StartupClass/1.Introduction_StartupClass/Controllers/HomeController.cs
+++ 1.Introduction_StartupClass/1.Introduction_StartupClass/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace _1.Introduction._Startup.Controllers
+namespace _1.Introduction_StartupClass.Controllers
 {
     public class HomeController : Controller
     {
--- 1.Introduction_StartupClass/1.Introduction_StartupClass/Program.cs
+++ 1.Introduction_StartupClass/1.Introduction_StartupClass/Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace _1.Introduction._Startup
+namespace _1.Introduction_StartupClass
 {
     public class Program
     {
