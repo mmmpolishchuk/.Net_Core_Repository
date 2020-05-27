44f1714999f375941aac7ea0a4d346832da8c940
Михайло Поліщук <55458556+mmmpolishchuk@users.noreply.github.com>
2020-05-27 23:43:25 +0300

    ы

--- 4.Dependency injection/DI. FortuneTeller/Repository/IFortuneGetter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DI._FortuneTeller.Repository
-{
-    public interface IFortuneGetter
-    {
-        public string GetFortune();
-    }
-}
--- 4.Dependency injection/Infestation reports/Models/Repositories/INewsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Infestation_reports.Models.Repositories
-{
-    public interface INewsRepository
-    {
-        public IEnumerable<News> GetAllNews();
-    }
-}
