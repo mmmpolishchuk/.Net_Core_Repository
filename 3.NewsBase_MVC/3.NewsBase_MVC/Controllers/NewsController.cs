d7833b56d46ff8ecd6534b2f85a5d71ce54acdf9
Михайло Поліщук <55458556+mmmpolishchuk@users.noreply.github.com>
2020-05-27 23:41:10 +0300

    Uploaded homework №3

---
+++ 3.NewsBase_MVC/3.NewsBase_MVC/Controllers/NewsController.cs
@@ -0,0 +1,14 @@
+﻿using _3.NewsBase_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3.NewsBase_MVC.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["news"] = NewsBase.News;
+            return View();
+        }
+    }
+}
