caf78ff15aa2f211323b7754761f16a3ce602ec4
Михайло Поліщук <55458556+mmmpolishchuk@users.noreply.github.com>
2020-05-27 23:11:39 +0300

    Uploaded homework №1

---
+++ 1.Introduction_StartupClass/1.Introduction_StartupClass/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1.Introduction_StartupClass.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Index method from HomeController class was called.";
+        }
+    }
+}
