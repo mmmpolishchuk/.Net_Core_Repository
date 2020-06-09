ebc00ff48c20bc772bd9ab72cc936418a6bd147e
Михайло Поліщук <55458556+mmmpolishchuk@users.noreply.github.com>
2020-06-09 21:53:44 +0300

    Uploaded homework №7

---
+++ 7. REST_API_example/Models/INewsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+using System.Web.Http.OData;
+
+namespace REST_API_example.Models
+{
+    public interface INewsRepository
+    {
+        public IEnumerable<News> GetAllNews(bool isFake);
+        void CreateNews(News news);
+        void DeleteNews(int id);
+        void MoveNews(int id, News anotherNews);
+        void UpdateNews(int id, News news);
+    }
+}
